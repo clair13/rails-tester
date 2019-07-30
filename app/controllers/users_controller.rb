f1497c7721ff3a7ae6beceeb3f1a7d756a681007
clair13 <theclairvoyant1313@yahoo.gr>
2019-07-30 15:27:06 +0300

    Implement new action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController<ApplicationController
   def index
     @name = "I am the Index action!"
   end
+
+  def new
+    @name = "I am the New action "
+  end
 end
--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -4,3 +4,17 @@ require 'rest-client'
 url = "http://localhost:3000/users"
 
 puts RestClient.get(url)
+
+
+url1 = "http://localhost:3000/users/new"
+
+puts RestClient.get(url1)
+
+=begin 
+  users GET    /users(.:format)                                                                         users#index
+                          POST   /users(.:format)                                                                         users#create
+                 new_user GET    /users/new(.:format)                                                                     users#new
+                edit_user GET    /users/:id/edit(.:format)                                                                users#edit
+                     user GET    /users/:id(.:format)      
+
+end=
